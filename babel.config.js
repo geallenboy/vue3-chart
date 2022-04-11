9cabfcee5a19c9c6cf9a8af0eb11b09c1f96ed38
geallenboy <gejialun88@163.com>
2022-04-11 17:12:39 +0800

    feat: rollup lib

--- babel.config.js
+++ babel.config.js
@@ -1,38 +1,5 @@
 module.exports = {
-  presets: [
-    [
-      '@vue/babel-preset-app',
-      {
-        modules: false,
-        corejs: 3,
-        jsx: false,
-        absoluteRuntime: false,
-        targets: {
-          browsers: 'last 2 versions, IE >= 11'
-        }
-      }
-    ],
-    '@babel/preset-typescript'
-  ],
+
   plugins: ['@vue/babel-plugin-jsx'],
-  env: {
-    commonjs: {
-      presets: [
-        [
-          '@vue/babel-preset-app',
-          {
-            modules: 'commonjs',
-            corejs: 3,
-            jsx: false,
-            absoluteRuntime: false,
-            targets: {
-              browsers: 'last 2 versions, IE >= 11'
-            }
-          }
-        ],
-        '@babel/preset-typescript'
-      ],
-      plugins: ['@vue/babel-plugin-jsx']
-    }
-  }
+  
 };
